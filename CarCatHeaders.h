2975b44d5d5930f6dff9092718d3855bae6b4349
Cavan Theiss <cavanrt@gmail.com>
2026-05-20 18:58:16 -0500

    Update CarCatHeaders.h

--- CarCatHeaders.h
+++ CarCatHeaders.h
@@ -1,1 +1,19 @@
-
+#ifndef 
+#define CarCatHeaders_h
+//***********************************
+#include <algorithm>
+#include <iostream>
+#include <stdlib.h>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <limits>
+#include <cctype>
+#include "CatFunc.h"
+#include "Cars.h"
+#include "UsePuts.h"
+#include "ValIndex.h"
+#include "Savld.h"
+//***********************************
+#endif CarCatHeaders_h
