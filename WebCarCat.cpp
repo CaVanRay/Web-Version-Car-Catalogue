a55fff43fdfcba2fdc570e8978904504f0ed59bc
Cavan Theiss <cavanrt@gmail.com>
2026-05-20 18:56:56 -0500

    Update WebCarCat.cpp

--- WebCarCat.cpp
+++ WebCarCat.cpp
@@ -1,1 +1,20 @@
+#include "CarCatHeaders.h"
 
+//**********************************************************************************************************************
+ 
+int main() {
+
+    car* head = nullptr;
+    car* tail = nullptr;
+
+    std::vector<car*> carCatalogue;
+
+    loadCatalogue(head, tail, carCatalogue);
+
+    printCatalogue(head);
+
+    optionsMenu(head, tail, carCatalogue);
+
+    return 0;
+}
+//**********************************************************************************************************************
